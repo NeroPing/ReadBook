735ff8ceea1c05d20f3d28034eeed0efa862075c
Nero <897887463@qq.com>
2018-12-11 11:26:29 +0800

    git Test

---
+++ src/controller/PinYinController.java
@@ -0,0 +1,12 @@
+package controller;
+import net.sourceforge.pinyin4j.PinyinHelper;
+
+public class PinYinController {
+    public static void main(String[] args){
+        char cStr = '我';
+        String[] cStrHY = PinyinHelper.toHanyuPinyinStringArray(cStr);
+        for (String s : cStrHY) {
+            System.out.println("pinyin  " + s);
+        }
+    }
+}
