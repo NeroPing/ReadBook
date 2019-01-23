a9b7798f706e633d0be7d823fe0d5f66b5998a96
Nero <897887463@qq.com>
2019-01-23 13:41:31 +0800

    Test

--- src/controller/PinYinController.java
+++ src/controller/PinYinController.java
@@ -1,43 +1,53 @@
 package controller;
-import net.sourceforge.pinyin4j.PinyinHelper;
-
-
-import java.io.IOException;
 
 import com.jacob.activeX.ActiveXComponent;
 import com.jacob.com.Dispatch;
 import com.jacob.com.Variant;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class PinYinController {
-    public static void main(String[] args){
-        char cStr = '我';
-        String[] cStrHY = PinyinHelper.toHanyuPinyinStringArray(cStr);
-        for (String s : cStrHY) {
-            System.out.println("pinyin  " + s);
 
+  public static void main(String[] args) {
+    ActiveXComponent sap = new ActiveXComponent("Sapi.SpVoice");
+
+    Dispatch sapo = sap.getObject();
+
+    // 音量 0-100
+    sap.setProperty("Volume", new Variant(100));
+    // 语音朗读速度 -10 到 +10
+    sap.setProperty("Rate", new Variant(2));
+
+    File file = new File("test.txt");
+    if (file.exists()) {
+      InputStreamReader reader = null;
+      BufferedReader in = null;
+      try {
+        reader = new InputStreamReader(new FileInputStream(file));
+        in = new BufferedReader(reader);
+        while (true) {
+          String str = in.readLine();
+          if (str == null) {
+            break;
+          }
+          // 执行朗读
+          Dispatch.call(sapo, "Speak", new Variant(str));
         }
-
-
-        ActiveXComponent sap = new ActiveXComponent("Sapi.SpVoice");
-
-        Dispatch sapo = sap.getObject();
+      } catch (Exception e) {
+        e.printStackTrace();
+      } finally {
+        sapo.safeRelease();
+        sap.safeRelease();
         try {
-
-            // 音量 0-100
-            sap.setProperty("Volume", new Variant(100));
-            // 语音朗读速度 -10 到 +10
-            sap.setProperty("Rate", new Variant(2));
-
-
-            // 执行朗读
-            Dispatch.call(sapo, "Speak", new Variant("汪**，你是个大傻逼"));
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            sapo.safeRelease();
-            sap.safeRelease();
+          in.close();
+        } catch (IOException e) {
+          e.printStackTrace();
         }
+      }
     }
+  }
 }
